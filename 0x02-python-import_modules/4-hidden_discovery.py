b4fe8faedcddf2fe0f7046f9459d3a76816b43a4
Samuel <njorogemuchoki15@gmail.com>
2023-06-08 22:34:15 +0000

    Prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
+    first_letter = '_'
     for name in dir(hidden_4):
-        print(name.strip('_'))
+        if (name[0] != first_letter) and (name[1] != first_letter):
+            print(name)

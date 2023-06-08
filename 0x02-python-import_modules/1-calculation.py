43cca37bb70e9f19b8bfc5749282132a2e22cb8f
Samuel <njorogemuchoki15@gmail.com>
2023-06-08 18:46:51 +0000

    Used format() to print

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
 if __name__ == "__main__":
-    print(f"{a:d} + {b:d} = {add(a, b):d}")
-    print(f"{a:d} - {b:d} = {sub(a, b):d}")
-    print(f"{a:d} * {b:d} = {mul(a, b):d}")
-    print(f"{a:d} / {b:d} = {div(a, b):d}")
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))

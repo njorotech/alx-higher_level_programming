c74617e4f02509bef495646419d088e73d09a234
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-06-12 16:03:48 +0000

    palindrome check

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * is_palindrome - checks if a singly linked list is a palindrome
+  * @head: the list to be checked
+  *
+  * Return: 1 if palindrome, 0 if not
+  */
+int is_palindrome(listint_t **head)
+{
+	return (0);
+}

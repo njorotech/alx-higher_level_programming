ed64778e44e7c521bfb10d77219054fce4011157
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-06-14 15:58:23 +0000

    Weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    sum = 0
-    length = len(my_list)
+    weight = 0
+    score = 0
     if len(my_list) == 0:
         return 0
     for i in my_list:
-        sum += i[1]
-    return sum / length
+        score += (i[0] * i[1])
+        weight += i[1]
+    return score / weight

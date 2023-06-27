d935e9fdb5359cba31550fc1b9292efc3c0cd4af
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-06-27 19:08:15 +0000

    An empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,9 @@
 #!/bin/usr/python3
+
 """My first square
 
 An empty class Square that defines a square
 """
 class Square:
-    """An empty square class
-
-    defines a square
-    """
-
+    """An empty class Square that defines a square"""
     pass

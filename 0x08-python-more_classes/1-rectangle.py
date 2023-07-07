d9572ad90635858ea3c0bdd5afdda6485bee5622
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-07-07 17:23:36 +0000

    Real defination of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
         """A method to set the width of the object"""
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        elif value <  0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         else:
             self.__width = value

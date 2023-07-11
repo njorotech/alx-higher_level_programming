22d5896fa2dde5152a0a76adbca0d930a8fd4457
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-07-11 18:25:14 +0000

    write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,8 @@
 
 
 def write_file(filename="", text=""):
+    ''' writes a string to a text file'''
+
     with open(filename, mode='w', encoding='utf-8') as myfile:
         characters = myfile.write(text)
     return characters

2975a374742580966e6a8fa8507ffc8fb42a88f6
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-07-11 18:11:14 +0000

    write to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+'''Write to file'''
+
+
+def write_file(filename="", text=""):
+    with open(filename, mode='w', encoding='utf-8') as myfile:
+        print(myfile.write(text))

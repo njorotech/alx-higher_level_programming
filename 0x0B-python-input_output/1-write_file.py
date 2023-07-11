dcd95c8e42c382c0e24ff9e0c99ba354d02e1280
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-07-11 18:21:59 +0000

    write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@
 def write_file(filename="", text=""):
     with open(filename, mode='w', encoding='utf-8') as myfile:
         characters = myfile.write(text)
-        return characters
+    return characters

96d579c1b190cf95b950a98bee6aca424d1702ee
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-07-11 17:55:15 +0000

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''Reads UTF8'''
+
+
+def read_file(filename=""):
+    '''reads a text file and prints it to stdout'''
+
+    with open(filename, encoding='utf-8') as myfile:
+        print(myfile.read())

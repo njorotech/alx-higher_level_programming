a3236fa65a4cbf0bde61e5c0568b3850ca15a0c2
Samuel Muchoki <njorogemuchoki15@gmail.com>
2023-07-17 09:44:43 +0000

    Python almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,7 +24,12 @@ class Rectangle(Base):
     def width(self, width):
         """Set the rectangle's width"""
 
-        self.__width = width
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
 
     @property
     def height(self):
@@ -36,7 +41,12 @@ class Rectangle(Base):
     def height(self, height):
         """set the rectangle's height"""
 
-        self.__height = height
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
 
     @property
     def x(self):
@@ -48,7 +58,12 @@ class Rectangle(Base):
     def x(self, x):
         """setting the value of x"""
 
-        self.__x = x
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be > 0")
+        else:
+            self.__x = x
 
     @property
     def y(self):
@@ -60,4 +75,50 @@ class Rectangle(Base):
     def y(self, y):
         """setting the value of y"""
 
-        self.__y = y
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be > 0")
+        else:
+            self.__y = y
+
+    def area(self):
+        """returns the area of a rectangle"""
+
+        return self.__width * self.__height
+
+    def display(self):
+        """prints in stdout the Rectangle instance with character #"""
+        for k in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for k in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print('#', end='')
+            print()
+
+    def __str__(self):
+        """String representation"""
+
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute"""
+
+        args_no = len(args)
+        if args and args_no > 0:
+            if args_no > 0:
+                self.id = args[0]
+            if args_no > 1:
+                self.__width = args[1]
+            if args_no > 2:
+                self.__height = args[2]
+            if args_no > 3:
+                self.__x = args[3]
+            if args_no > 4:
+                self.__y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+

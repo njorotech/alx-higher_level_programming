8db910821e3ae1fd89ebd03143aa196bf1b62088
Samuel <njorogemuchoki15@gmail.com>
2023-09-04 19:14:38 +0000

    String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+"""String representation"""
+
+
+class Rectangle:
+    """A class that defines a rectangle based on width and height"""
+
+    def __init__(self, width=0, height=0):
+        """Intializes an instance of the Rectangle class"""
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Retrieves the width of the rectangle"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the height of the rectangle"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Computes the rectangle area"""
+
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """computes the rectangle perimeter"""
+
+        if (self.width == 0) or (self.height == 0):
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """string representation of the object"""
+
+        if (self.__width == 0) or (self.__height == 0):
+            empty_string = ""
+            return empty_string
+        else:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    print("#", end="")
+                print()

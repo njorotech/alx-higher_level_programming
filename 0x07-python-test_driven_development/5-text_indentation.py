233e1d5c49fae945777ea1b4e664d271564aee4a
Samuel <njorogemuchoki15@gmail.com>
2023-09-06 14:59:28 +0000

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,3 +11,10 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    for c in text:
+        if (c == ".") or (c == "?") or (c == ":"):
+            print(c)
+            print()
+        else:
+            print(c, end="")
+

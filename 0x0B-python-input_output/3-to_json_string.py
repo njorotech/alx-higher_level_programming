cf5be63bdb3006e44a1ef756fa95ee4929f162ad
Samuel <njorogemuchoki15@gmail.com>
2023-09-12 20:23:03 +0000

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""To JSON string module"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the json representation of an object"""
+
+    return json.dumps(my_obj)

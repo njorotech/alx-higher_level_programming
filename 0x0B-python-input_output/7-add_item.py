ad9f3d52efd57afcbeefc161824e9c7e6f086941
Samuel <njorogemuchoki15@gmail.com>
2023-09-12 22:38:06 +0000

    Load, add ,save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,14 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-arguments = sys.argv[1:]
-save_to_json_file(arguments, 'add_item.json')
-load_from_json_file('add_item.json')
+if __name__ == "__main__":
+    try:
+        arguments_list = load_from_json_file('add_item.json')
+    except Exception:
+        arguments_list = []
+
+    for arg in sys.argv[1:]:
+        arguments_list.append(arg)
+
+    save_to_json_file(arguments_list, 'add_item.json')
+    load_from_json_file('add_item.json')

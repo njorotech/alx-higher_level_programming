860e9402d65e044a2f0738f6228583c64ebd9cbd
Samuel <njorogemuchoki15@gmail.com>
2023-09-16 12:13:58 +0000

    Display #

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,3 +106,9 @@ class Rectangle(Base):
 
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """String representation of the Rectangle class"""
+
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                self.__y, self.__width, self.__height)

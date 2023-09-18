d71164465c1ed9058ab7a57fd5516877843b10ec
Samuel <njorogemuchoki15@gmail.com>
2023-09-18 12:11:36 +0000

    JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,12 @@ class Base:
             with open(file_name, 'w', encoding="utf-8") as f:
                 dict_list = [obj.to_dictionary() for obj in list_objs]
                 f.write(cls.to_json_string(dict_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation"""
+
+        if (json_string is None) or (json_string == ""):
+            return {}
+        else:
+            return json.loads(json_string)

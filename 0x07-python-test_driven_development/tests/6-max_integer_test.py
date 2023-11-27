51415f1f11c8efb2676aff389787bc16fe8ee2ca
Samuel <njorogemuchoki15@gmail.com>
2023-11-27 11:07:54 +0000

    Max integer - Unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,3 +13,8 @@ class TextMaxInteger(unittest.TestCase):
         """function to test max_integer funtion"""
 
         self.assertEqual(max_integer([1, 2, 3, 5]), 5)
+        self.assertEqual(max_integer([6, 1, 2, 3, 5]), 6)
+        self.assertEqual(max_integer([1, 2, 7, 3, 5]), 7)
+        self.assertEqual(max_integer([1, 2, -3, 5]), 5)
+        self.assertEqual(max_integer([-1, -2, -3, -5]), -1)
+        self.assertEqual(max_integer([-1, 2, 3, -5]), 3)

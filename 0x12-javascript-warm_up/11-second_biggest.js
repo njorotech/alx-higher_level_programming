1884d36109c306c40213cdc543c606e1bc547194
Samuel <njorogemuchoki15@gmail.com>
2024-01-09 10:07:03 +0000

    second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const args = process.argv.length;
+const argsList = process.argv;
+if (args === 2) {
+  console.log(0);
+} else if (args === 3) {
+  console.log(0);
+} else {
+  let big = 0;
+  let second = 0;
+  for (let counter = 2; counter <= args; counter++) {
+    const argValue = Number(argsList[counter]);
+    if (argValue > big) {
+      big = argValue;
+    }
+  }
+  for (let i = 2; i <= args; i++)
+  {
+    const argsValue = Number(args_list[i]);
+    if (argsValue > second && argsValue < big) {
+      second = argsValue;
+    }
+  }
+  console.log(second);
+}

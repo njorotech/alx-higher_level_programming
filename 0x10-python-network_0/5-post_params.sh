14d83e18c10d0796b4191420c646b29f25f7a314
Samuel <njorogemuchoki15@gmail.com>
2024-01-31 08:11:38 -0800

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request to the passed URL, and displays the body of the response
-curl -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}' -H "Content-Type: application/json" "$1"
+curl -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"

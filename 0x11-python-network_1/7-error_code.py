fa5dfbd70459381b4bf9a502c7dae9d6b4f934a1
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-02-03 08:00:44 -0800

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays the body of the response.
-If the HTTP status code is greater than or equal to 400, print: Error code: followed by the value of the HTTP status code
+"""takes in a URL, sends a request to the URL and displays the body of the
+response. If the HTTP status code is greater than or equal to 400, print:
+Error code: followed by the value of the HTTP status code
 """
 
 import requests
